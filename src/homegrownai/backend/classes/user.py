d2a9cb71df5d1a5dbf2f76762a7bbf239a9c19b9
Luke Hofstetter <ldhofstetter@gmail.com>
2026-07-06 10:28:29 -0700

    [feat] add add_user, delete_user
    Fleshes out the User class more by adding UUIDs that are randomly
    generated.
    
    Additionally, adds add_user and delete_user functions to allow for
    modifications to the database.

--- src/homegrownai/backend/classes/user.py
+++ src/homegrownai/backend/classes/user.py
@@ -1,20 +1,46 @@
 from datetime import date
-from typing import ByteString, Union
+from typing import ByteString
+from uuid import UUID as UUID_Rand
+from uuid import uuid4
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship, Session 
+from sqlalchemy import select, Engine, insert
+from sqlalchemy.dialects.postgresql import UUID
+from pydantic import SecretStr
 
 from .role import Role
-from .db import Base
+from .db import Base, DB, DBSession
+from .exceptions import UserRegistrationError, EmailAlreadyRegisteredError
 
-from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class User(Base):
     __tablename__ = "users"
     
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(
+            UUID(as_uuid=True),
+            primary_key=True,
+            default=uuid4
+        )
     username: Mapped[str]
+    hashed_password: Mapped[SecretStr]
     email: Mapped[str]
     registration_date: Mapped[date]
-    roles: Mapped[list[Role]] = relationship(back_populates="user")
-    
-    def add_role(self, admin_token: ByteString, user_id: Union[int, None] = None) -> bool:
-        return True
-    
+       
+
+def add_user(database: DB, new_user: User):
+    with DBSession(database) as session:
+        objects = session.scalars(select(User).where(User.email == new_user.email)).all()
+        
+        if len(objects) != 0:
+            raise EmailAlreadyRegisteredError()
+        else:
+            session.add(new_user)
+
+def delete_user(database: DB, existing_user: User):
+    with DBSession(database) as session:
+        user = session.scalars(select(User).where(User.email == existing_user.email)).first()
+        
+        if user is None:
+            raise EmailAlreadyRegisteredError()
+        else:
+            session.delete(user)
