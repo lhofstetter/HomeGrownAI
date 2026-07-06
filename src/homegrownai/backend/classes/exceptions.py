157d5e41f6107f1ac7118141f805d9d97670da0f
Luke Hofstetter <ldhofstetter@gmail.com>
2026-07-06 10:21:18 -0700

    [feat] add exceptions
    Adds application-specific exceptions to make it easier to understand
    what went wrong. Additionally, FastAPI lets you tie your custom
    exceptions to actual status codes, which should make it easier when I
    start building the frontend.

--- src/homegrownai/backend/classes/exceptions.py
+++ src/homegrownai/backend/classes/exceptions.py
@@ -1,9 +1,32 @@
 class AIAppError(Exception):
-    def __init__(self, message, status_code):
+    """
+    Base exception for HomeGrownAI. You should never need to use this exception directly, rather, you should use one of the exceptions below. 
+    
+    If an appropriate exception does *not* already exist, you may define one, but it must either inherit from this class OR have a good 
+    reason for inheriting from a subclass (e.g., you want to be more specific about the TYPE of DatabaseError you get, so you implement
+                                           a DatabaseReadError for when SELECT returns an error).
+    """
+    def __init__(self, message):
         super().__init__(message)
-        self.status_code = status_code
-
 
 class InsufficientPermissions(AIAppError):
     def __init__(self):
-        super().__init__("Insufficient Permissions. Please contact your administrator if you believe this to be an error.", 20)
+        super().__init__("Insufficient Permissions.")
+
+class DatabaseError(AIAppError):
+    def __init__(self):
+        super().__init__("Error when attempting to read or write to the database.")
+
+class UserRegistrationError(AIAppError):
+    def __init__(self):
+        super().__init__("Error when attempting to register a new user.")
+
+class UserNotFoundError(AIAppError):
+    def __init__(self):
+        super().__init__("Error when attempting to locate a user when no such user exists.")
+
+class EmailAlreadyRegisteredError(UserRegistrationError):
+    pass
+
+class UserDeletionError(UserNotFoundError):
+    pass
