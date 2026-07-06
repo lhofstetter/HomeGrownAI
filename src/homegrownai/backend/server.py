64e840a803cf87557949cf65ffb31c5da9adb623
Luke Hofstetter <ldhofstetter@gmail.com>
2026-07-06 10:30:58 -0700

    [chore] clean server.py, update uv.lock
    Adds uv.lock that should've been committed in the last couple of commits
    but I'm too lazy to rebase right now.
    
    Also, removes most of src/homegrownai/backend/server.py, in preparation
    for defining real API routes that are files under a dedicated API
    folder and used by FastAPI via include_router function.

--- src/homegrownai/backend/server.py
+++ src/homegrownai/backend/server.py
@@ -1,26 +1,28 @@
-from functools import lru_cache
+from importlib.metadata import version, PackageNotFoundError
 
 from sqlalchemy import create_engine
-from typing import Annotated
-
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, status
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.responses import JSONResponse
 
 from homegrownai.backend.classes.settings import settings
 
-app = FastAPI()
+from .classes.db import DB
 
-db = create_engine(settings.db_url)
+app = FastAPI(
+    title="HomeGrownAI API Backend",
+    version= version("homegrownai")
+)
+
+db = DB(settings.db_url, settings.db_user, settings.db_passwd)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
-async def default():
-    raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Unknown route."
-        )
+async def root():
+    return {
+        "status": "ok",
+        "version": app.version,
+    }
 
-@app.post("/login")
-async def login():
-    pass
+
